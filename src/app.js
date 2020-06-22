ba1d1a1a20bb767892253023244e06e3f3ced184
Amardeep Singh Suri <amardeepsinghsuri@gmail.com>
2020-06-22 14:44:47 +0200

    File handling and Routing

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const app = express();
+const app = new express();
 const port = 3000;
 
-const accountData = fs.readFileSync("src/json/accounts.json", "utf8");
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), "utf8");
 const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync("src/json/users.json", "utf8");
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), "utf8");
 const users = JSON.parse(userData);
 
 app.set('views', path.join(__dirname, 'views')); 
@@ -15,10 +15,12 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res,) {res.render('index', {title: 'Account Summary', accounts: 'accounts'}); });
-app.get('/savings', function(req, res,) {res.render('account', {account: 'accounts.savings'}); });
-app.get('/checking', function(req, res,) {res.render('account', {account: 'accounts.checking'}); });
-app.get('/credit', function(req, res,) {res.render('account', {account: 'accounts.credit'}); });
+app.get('/', function(req, res,) {res.render('index', {title: 'Account Summary', accounts: accounts}); });
+
+app.get('/savings', function(req, res,) {res.render('account', {account: accounts.savings}); });
+app.get('/checking', function(req, res,) {res.render('account', {account: accounts.checking}); });
+app.get('/credit', function(req, res,) {res.render('account', {account: accounts.credit}); });
+
 app.get('/profile', function(req, res,) {res.render('profile', {user: users[0]}); });
 
 app.listen(port, () => console.log(`PS Project Running on port ${port}`));
