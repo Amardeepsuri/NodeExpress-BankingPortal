20f97f09a5ad6c436b9fcfaed6ee1352ae290b40
Amardeep Singh Suri <amardeepsinghsuri@gmail.com>
2020-06-22 14:28:07 +0200

    File handling and Routing

--- src/app.js
+++ src/app.js
@@ -4,11 +4,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const accountData = fs.readFileSync("src/json/accounts.json", "utf8");
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync("src/json/users.json", "utf8");
+const users = JSON.parse(userData);
+
 app.set('views', path.join(__dirname, 'views')); 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res,) {res.render('index', {title: 'Index'}); });
+app.get('/', function(req, res,) {res.render('index', {title: 'Account Summary', accounts: 'accounts'}); });
+app.get('/savings', function(req, res,) {res.render('account', {account: 'accounts.savings'}); });
+app.get('/checking', function(req, res,) {res.render('account', {account: 'accounts.checking'}); });
+app.get('/credit', function(req, res,) {res.render('account', {account: 'accounts.credit'}); });
+app.get('/profile', function(req, res,) {res.render('profile', {user: users[0]}); });
 
 app.listen(port, () => console.log(`PS Project Running on port ${port}`));
+
